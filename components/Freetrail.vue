9a3b6c3acdabcf5ee98388946b03cb8d52ac7867
Chadipiralla Sneha <77140375+snehareddy24@users.noreply.github.com>
2021-08-17 15:32:38 +0530

    Update Freetrail.vue

--- components/Freetrail.vue
+++ components/Freetrail.vue
@@ -3,7 +3,7 @@
     <div class="container py-3">
         <header>
             <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-                <nuxt-link to="#" class="d-flex align-items-center text-dark text-decoration-none">
+                <nuxt-link to="/" class="d-flex align-items-center text-dark text-decoration-none">
 
                     <span class="fs-4">STREAMX</span>
                 </nuxt-link>
